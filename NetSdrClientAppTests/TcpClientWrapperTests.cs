628cc962b525b685a5ce4057fc285f5d7e6565bf
AnTiSwitch <ukrainckiukazak@gmail.com>
2025-11-16 16:27:46 +0200

    lab1

--- NetSdrClientAppTests/TcpClientWrapperTests.cs
+++ NetSdrClientAppTests/TcpClientWrapperTests.cs
@@ -1,26 +1,26 @@
 using System;
 using System.Threading.Tasks;
 using NetSdrClientApp.Networking;
-using NUnit.Framework; 
+using NUnit.Framework;
 
 namespace NetSdrClientAppTests
 {
     public class TcpClientWrapperTests
     {
-        [Test] 
+        [Test]
         public void SendMessageAsync_Bytes_WhenNotConnected_ThrowsInvalidOperationException()
+
         {
             // Arrange
             var tcpClient = new TcpClientWrapper("127.0.0.1", 8080);
             var testData = new byte[] { 1, 2, 3 };
 
             // Act & Assert
-            // (Це синтаксис NUnit для перевірки асинхронних винятків)
             Assert.ThrowsAsync<InvalidOperationException>(async () =>
                 await tcpClient.SendMessageAsync(testData));
         }
 
-        [Test] 
+        [Test]
         public void SendMessageAsync_String_WhenNotConnected_ThrowsInvalidOperationException()
         {
             // Arrange
